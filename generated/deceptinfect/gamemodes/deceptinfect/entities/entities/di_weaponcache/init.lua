49d484bc0e33c5d3558554372dec33bb972c0507
Dizbdeedee <dizbee@mentice.cotse.net>
2021-06-19 16:02:48 +0000

    Stopped tracking generated files, +1 build step
    FossilOrigin-Name: 7c00f61a6477ea6fabb462579e995d9a7d1c5964335238d9fd3775942576c459

--- generated/deceptinfect/gamemodes/deceptinfect/entities/entities/di_weaponcache/init.lua
+++
@@ -1,9 +0,0 @@
-ENT.Base = "base_anim"
-ENT.Type = "anim"
-AddCSLuaFile("cl_init.lua")
-
-function ENT:Initialize()
-	local ent = deceptinfect_HAXE_EXPORT.di_weaponcache.new(self)
-	self._gHaxeBurrow = ent
-end
-
