755cd5b5a3e27d4938a12e9d465155c478082e59
Dizbdeedee <dizbee@mentice.cotse.net>
2021-06-19 15:48:49 +0000

    rewind
    FossilOrigin-Name: 01fe959aef45f4ba518443c52d508ae84d3f9087d63b8cb79da18fc39451f97b

--- src/deceptinfect/client/ClientOverrides.hx
+++ src/deceptinfect/client/ClientOverrides.hx
@@ -1,5 +1,6 @@
 package deceptinfect.client;
 
+import gmod.helpers.LuaArray;
 import gmod.enums.BUTTON_CODE;
 import deceptinfect.game.RagdollSystem;
 import deceptinfect.ecswip.GrabSystem;
@@ -8,7 +9,7 @@ import deceptinfect.GEntCompat.GPlayerCompat;
 import deceptinfect.infection.InfectionComponent;
 import deceptinfect.infection.InfectedComponent;
 import deceptinfect.ecswip.ComponentManager;
-
+import gmod.gclass.Vector;
 #if client
 class ClientOverrides extends gmod.helpers.gamemode.GMBuild<gmod.gamemode.GM> {
 	override function HUDPaint() {
@@ -23,6 +24,7 @@ class ClientOverrides extends gmod.helpers.gamemode.GMBuild<gmod.gamemode.GM> {
 	override function PlayerButtonDown(ply:Player, button:BUTTON_CODE) {
 		switch (button) {
 			case KEY_L:
+				trace("l key says hello!");
 			// var sab = SabotagePanel.inst;
 			// switch (sab.IsVisible()) {
 			//     case true:
@@ -36,8 +38,115 @@ class ClientOverrides extends gmod.helpers.gamemode.GMBuild<gmod.gamemode.GM> {
 	}
 
 	override function PreDrawOpaqueRenderables(isDrawingDepth:Bool, isDrawSkybox:Bool):Bool {
-		GrabSystem.drawCylinders();
+		// RenderLib.SetColorMaterial();
+		// RenderLib.SetColorModulation(0,0,0);
+		// GrabSystem.drawCylinders();
+		
 		return null;
+	}
+
+	var vectors:LuaArrayFast<Vector> = null;
+	function getVectors() {
+		if (vectors == null) {
+			vectors = new LuaArrayFast();
+			var tbl:AnyTable = untyped GameLib.GetWorld().GetBrushSurfaces();
+			for (si in tbl) {
+				// final si:Dynamic = exdee;
+				// final ftbl:LuaArrayFast<Vector> = new LuaArrayFast();
+				final vtbl:LuaArray<Vector> = si.GetVertices();
+				// final isnodraw:Bool = si.IsNoDraw();
+				// if (isnodraw) continue;
+				final len = vtbl.length();
+				final remain = len % 3;
+				final upto = Math.floor(len / 3);
+				
+				if (len < 3) {
+					continue;
+				}
+				if (remain == 0) {
+					DebugoverlayLib.Cross(vtbl[1],2,120,Gmod.Color(255,0,0));
+					DebugoverlayLib.Cross(vtbl[vtbl.length()],5,120,Gmod.Color(0,255,0));
+					// DebugoverlayLib.Cross(vtbl[1],2,120,Gmod.Color(255,0,0));
+					// DebugoverlayLib.Cross(vtbl[vtbl.length()],5,120,Gmod.Color(0,255,0));
+				} else if (remain == 1) {
+					
+					// DebugoverlayLib.Cross(vtbl[1],2,120,Gmod.Color(0,255,0));
+				} else {
+					
+				}
+				
+				DebugoverlayLib.Text(vtbl[1],Gmod.tostring(vtbl.length()),120,false);
+				if (len == 3) {
+					vectors.add(vtbl[1]);
+					vectors.add(vtbl[3]);
+					vectors.add(vtbl[2]);
+					vectors.add(vtbl[2]);
+					continue;
+				}
+				vectors.add(vtbl[1]);
+				vectors.add(vtbl[3]);
+				vectors.add(vtbl[2]);
+				vectors.add(vtbl[2]);
+				// vectors.add(vtbl[1]);
+				// vectors.add(vtbl[3]);
+				// vectors.add(vtbl[2]);
+				// vectors.add(vtbl[2]);
+				for (i in 1...upto) {
+					vectors.add(vtbl[1]);
+					vectors.add(vtbl[i * 3]); //0
+					vectors.add(vtbl[i * 3 + 1]); //1 
+					vectors.add(vtbl[i * 3 + 2]); //2
+					vectors.add(vtbl[1]);
+					vectors.add(vtbl[i*3+1]);
+					vectors.add(vtbl[i*3+2]);
+					vectors.add(vtbl[i*3+3]);
+				}
+				if (remain == 1) {
+					vectors.add(vtbl[1]);
+					vectors.add(vtbl[upto*3]);
+					vectors.add(vtbl[upto*3+1]);
+					vectors.add(vtbl[upto*3+1]);
+				} else if (remain == 2) {
+					vectors.add(vtbl[1]);
+					vectors.add(vtbl[upto*3]);
+					vectors.add(vtbl[upto*3+1]);
+					vectors.add(vtbl[upto*3+2]);
+					// vectors.add(vtbl[1]);
+					// vectors.add(vtbl[upto*3+1]);
+					// vectors.add(vtbl[upto*3+2]);
+					// vectors.add(vtbl[upto*3+2]);
+				}
+			}
+
+		}
+		return vectors;
+	}
+	
+
+	override function PostDrawTranslucentRenderables(bDrawingDepth:Bool, bDrawingSkybox:Bool) {
+		if (bDrawingDepth || bDrawingSkybox) return;
+		// CamLib.Start3D();
+		// CamLib.IgnoreZ(true);
+		RenderLib.SetColorMaterial();
+		final getVec:LuaArrayFast<Vector> = getVectors();
+		var total = getVec.length;
+		var i = 1;
+		var c = Gmod.Color(0,0,0,200);
+		while (i <= total) {
+			
+			RenderLib.DrawQuad(getVec[i],getVec[i + 1],getVec[i + 2],getVec[i + 3],c);
+			i += 4;
+		}
+		// var gayness = 0;
+		// for (i in 1...untyped getVec._len) {
+		// 	RenderLib.DrawQuad(i)
+		// }
+		CamLib.IgnoreZ(false);
+		// CamLib.End3D();
+	}
+	
+	override function PostRender() {
+		
 	}
 
 	override function HUDDrawScoreBoard() {
@@ -53,4 +162,30 @@ class ClientOverrides extends gmod.helpers.gamemode.GMBuild<gmod.gamemode.GM> {
 		}
 	}
 }
+
+
+// @:forward
+// @:transitive
+// abstract IterateArr<T>(LuaArray<T>) to LuaArray<T> {
+
+// 	public var length(get,set):Int;
+
+// 	public inline function get_length() {
+// 		return untyped this.length;
+// 	}
+
+// 	public inline function set_length(x:Int) {
+// 		return untyped this.length = x;
+// 	}
+
+// 	public inline function iterator():T {
+// 		return new IntIterator(1,length);
+// 	}
+
+// 	public inline function add() {
+
+// 	}
+
+// }
+
 #end
