8b363c5449f072d68c7a036d85e934d7ab70181a
Dizbdeedee <dizbee@mentice.cotse.net>
2021-09-13 17:48:02 +0000

    cleanup imports
    FossilOrigin-Name: f45f08ea579ed48fde39e1ba1bf6ce796b86e682ede20ebe0f8e2516de9a1cdb

--- src/deceptinfect/import.hx
+++ src/deceptinfect/import.hx
@@ -9,7 +9,6 @@ import gmod.libs.*;
 import gmod.types.*;
 import lua.Table.AnyTable;
 import lua.Table;
-import lua.UserData;
 import haxe.extern.EitherType;
 import haxe.extern.Rest;
 import haxe.Constraints.Function;
@@ -19,17 +18,14 @@ import gmod.helpers.net.NET_Server;
 using gmod.helpers.PairTools;
 using gmod.helpers.TableTools;
 using tink.CoreApi;
+import deceptinfect.macros.IterateEnt;
 
 import gmod.Gmod.IsValid;
 import gmod.Gmod;
 import deceptinfect.ecswip.Component;
 import deceptinfect.ecswip.System;
-// import deceptinfect.ecswip.ComponentManager.entities;
 import deceptinfect.ecswip.ComponentManager;
 import deceptinfect.ecswip.SystemManager;
-// import deceptinfect.ecswip.SystemManager.getSystem;
-// import deceptinfect.ecswip.SystemManager.getSystem2;
-import deceptinfect.game.components.KeepRestart;
 using deceptinfect.util.MinMax;
 using deceptinfect.util.EntityExt;
 #end
