9a8fd76d0e0dd69bc7f2a1e677a7064e609d7a77
Dizbdeedee <dizbee@mentice.cotse.net>
2023-06-24 16:14:03 +0000

    Add forgotten components. Think of a new commit style
    FossilOrigin-Name: 81e32fdfa192d783e0c63f31c30d96d5c94bbef118bfbf01b1cdb32f51cb2267

---
+++ src/deceptinfect/game/components/GamePlayer.hx
@@ -0,0 +1,5 @@
+package deceptinfect.game.components;
+
+class GamePlayer extends Component {
+    var player:gmod.gclass.Player;
+}
---
+++ src/deceptinfect/game/components/NotInGamePlayer.hx
@@ -0,0 +1,5 @@
+package deceptinfect.game.components;
+
+class NotInGamePlayer extends Component {
+    var player:gmod.gclass.Player;
+}
