1baefc3eceb87c74379beb1e5422a3d344fd5736
Dizbdeedee <dizbee@mentice.cotse.net>
2023-11-10 08:22:41 +0000

    Fixup for dev. Import pass main.hx
    FossilOrigin-Name: bba4c40110da83968430b326ac4dc87f73c6df6405c0d4340862e8a3e2af72c9

--- src/Main.hx
+++ src/Main.hx
@@ -1,39 +1,19 @@
-import deceptinfect.ecswip.SystemManager.SystemManagerDef;
-import deceptinfect.ecswip.ComponentManager.ComponentManagerDef;
+//actual imports
+import deceptinfect.ecswip.SystemManager;
+import deceptinfect.ecswip.ComponentManager;
+import deceptinfect.DeceptInfect;
+import deceptinfect.Misc;
 import deceptinfect.game.GameSystem;
-import haxe.macro.Compiler;
-import sys.FileSystem;
-import haxe.Resource;
-import sys.io.File;
-import deceptinfect.lib.Mri;
-import tink.CoreApi.Callback;
-import gmod.gclass.Entity;
-import lua.Lua;
-import deceptinfect.lib.BSP;
-// import hxbit.NetworkHost;
-import gmod.stringtypes.EntityClass;
-import gmod.Gmod;
-import deceptinfect.GEntCompat.GPlayerCompat;
-import deceptinfect.game.SpawnSystem;
-import gmod.helpers.types.Vector;
-import gmod.libs.*;
-import deceptinfect.ecswip.*;
-import deceptinfect.ents.*;
 #if client
-import deceptinfect.client.SabotagePanel;
-#end
-import deceptinfect.*;
 import deceptinfect.client.ClientOverrides;
-import gmod.libs.PlayerLib;
-import deceptinfect.items.Weapon_Di_Scan;
-import deceptinfect.items.Di_cure;
-import deceptinfect.ents.Di_battery;
-import deceptinfect.ents.Di_barrel;
-import deceptinfect.ents.Di_puddle;
-import deceptinfect.ents.Di_spitball;
-import deceptinfect.weapons.Weapon_di_spit;
-import deceptinfect.game.SpawnSystemNav;
-import deceptinfect.Spread;
+#end
+
+import gmod.libs.UtilLib;
+import gmod.libs.HookLib;
+import gmod.libs.FileLib;
+import gmod.libs.GameLib;
+import gmod.libs.MathLib;
+import gmod.Gmod;
 
 class Main {
 
